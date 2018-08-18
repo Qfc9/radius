6bf43e9486af3bb22bcc508ec8dd48f7b370fc95
Elijah Harmon <elijahharmon@gmail.com>
2018-08-18 19:03:14 -0400

    Added retrieval of user info

--- Radius/SecondViewController.swift
+++ Radius/SecondViewController.swift
@@ -12,39 +12,45 @@ import UIKit
 import AWSAuthCore
 import AWSAuthUI
 import AWSMobileClient
+import AWSUserPoolsSignIn
 
 
 class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         if !AWSSignInManager.sharedInstance().isLoggedIn {
-//            presentAuthUIViewController()
+            //            presentAuthUIViewController()
         }
-    }
-    
-    func presentAuthUIViewController() {
-        let config = AWSAuthUIConfiguration()
-        config.enableUserPoolsUI = true
-//        config.addSignInButtonView(class: AWSFacebookSignInButton.self)
-//        config.addSignInButtonView(class: AWSGoogleSignInButton.self)
-//        config.backgroundColor = UIColor.blue
-        config.font = UIFont (name: "Helvetica Neue", size: 20)
-        config.isBackgroundColorFullScreen = true
-        config.canCancel = true
-//        let VC1 = self.storyboard!.instantiateViewController(withIdentifier: "theView")
         
-//        self.navigationController!.pushViewController(VC1, animated: true)
+        let identityManager = AWSIdentityManager.default()
+        usernameLabel.text = identityManager.identityId
         
-        AWSAuthUIViewController.presentViewController(
-            with: self.navigationController!,
-            configuration: config, completionHandler: { (provider: AWSSignInProvider, error: Error?) in
-                if error == nil {
-                    // SignIn succeeded.
+        let userFromPool = AWSCognitoUserPoolsSignInProvider.sharedInstance().getUserPool().currentUser()
+        
+        print(userFromPool?.username)
+
+        
+        userFromPool?.getDetails().continueOnSuccessWith(block: { (task) -> Any? in
+            DispatchQueue.main.async {
+                
+                if let error = task.error as NSError? {
+                    print("Error getting user attributes from Cognito: \(error)")
                 } else {
-                    // end user faced error while loggin in, take any required action here.
+                    let response = task.result
+                    if let userAttributes = response?.userAttributes {
+                        print("user attributes found: \(userAttributes)")
+                        for attribute in userAttributes {
+                            print("\(attribute.name) = \(attribute.value)")
+                        }
+                    }
                 }
+            }
         })
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
